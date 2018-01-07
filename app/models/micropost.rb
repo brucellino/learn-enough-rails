46d7643bd837b2366ca2bf5820ffe60ad8fce1ed
Bruce Becker <brucellino@gmail.com>
2018-01-07 06:39:41 +0100

    Remove microposts. They were a silly model. Hm. yes.

--- app/models/micropost.rb
+++
@@ -1,5 +0,0 @@
-class Micropost < ApplicationRecord
-    belongs_to :user
-    validates :user_id, :content, presence: true
-    validates :content, length: { maximum: 140, too_long: "%{count} characters is maximum allowed"}
-end
