2869b9ac0b3e0d012bbab8d3da0095429a24da6f
Bruce Becker <brucellino@gmail.com>
2018-01-09 10:11:34 +0100

    add tests for email addresses

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -46,4 +46,18 @@ class UserTest < ActiveSupport::TestCase
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid."
     end
   end
+
+  test "Duplicate users are invalid" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "Email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
 end
