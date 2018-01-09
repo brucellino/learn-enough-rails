1f91e90893c6da1c850f8df2352ff07a3ad3c06b
Bruce Becker <brucellino@gmail.com>
2018-01-09 16:46:55 +0100

    Add gravatar helper and update user show page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,12 @@
 module UsersHelper
+
+    # Returns the gravatar for the user
+    def gravatar_for(user, options = { size: 80 })
+        size = options[:size]
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?size=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "img img-rounded-circle")
+    end
+
+
 end
