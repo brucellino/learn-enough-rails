131f4d72b351898f6d0e89501bde8117a21cd7fc
Bruce Becker <brucellino@gmail.com>
2018-01-10 14:19:13 +0100

    Provide func and test for logged in user
    1. add jquery to application.js
    2. Update nav for logged in user
    3. provide logout and account buttons and paths
    4. update test fixtures with a good test user.
    5. provide tests for logging in invalid and valid users

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "Login with valid information" do
+
+  def setup
+    @user = users(:good)
+  end
+
+  test "Login with invalid information" do
     get login_path
     assert_template "sessions/new"
     post login_path, params: { session: {email: "", password: "" } }
@@ -12,6 +17,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     # should  disappear after one showing
     get root_path
     assert flash.empty?
+  end
 
+  test "Login in with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, 
+                                          password: 'good_password_good' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", user_path(@user)
   end
 end
